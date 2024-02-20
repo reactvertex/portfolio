fb4abf33c6d2ebaa89688d5368c4ff19f06c0314
reactvertex <sharmasandip2085@gmail.com>
2024-02-21 01:08:49 +0530

    added animation in contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,16 +1,26 @@
-import React from "react";
-import {FormContainerWrapper, ContactContent, ContactHeading, ContactDescription, ContanctWrapper,UserDetailsIcon,UserDetailsHeading,UserDetailsDetails ,UserDetailsContainer} from './styleComponent';
-import {userinfo} from '../../Constant/Contact';
+import React, {useEffect} from "react";
+import { FormContainerWrapper, ContactContent, ContactHeading, ContactDescription, ContanctWrapper, UserDetailsIcon, UserDetailsHeading, UserDetailsDetails, UserDetailsContainer } from './styleComponent';
+import { userinfo } from '../../Constant/Contact';
 import PageHeader from '../PageHeader';
 import ContactForm from "./contactForm";
 
 
 const ContactPage = () => {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = "https://static-bundles.visme.co/forms/vismeforms-embed.js";
+        script.async = false;
+        document.body.appendChild(script);
+        return () => {
+          document.body.removeChild(script);
+        }
+      }, []);
     return (
         <ContactContent className="row mx-0">
             <div className='col-lg-12 col-sm-12 m-0'>
                 <PageHeader title="CONTACT" subTitle1='GET IN' subTitle2='TOUCH' />
             </div>
+            {/* <script src="https://static-bundles.visme.co/forms/vismeforms-embed.js"></script> */}
             <FormContainerWrapper className='col-lg-5 col-sm-12 m-0'>
                 <div className="d-flex  justify-content-center">
                     <ContanctWrapper>
@@ -29,9 +39,10 @@ const ContactPage = () => {
                 </div>
             </FormContainerWrapper>
             <div className='col-lg-7 col-sm-12 m-0'>
-                <ContactForm />
+            <div class="visme_d" data-title="Untitled Project" data-url="8r6768yk-untitled-project" data-domain="forms" data-full-page="false" data-min-height="500px" data-form-id="18397"></div>
+                {/* <ContactForm /> */}
             </div>
         </ContactContent>
     )
 }
-export default ContactPage
+export default ContactPage;
