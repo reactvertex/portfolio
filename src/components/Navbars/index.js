23ecc0a5aef5c68f1747007e62b16a101fd33ebe
reactvertex <sharmasandeep2085@gmail.com>
2026-04-01 02:49:40 +0530

    fixed lint error

--- src/components/Navbars/index.js
+++ src/components/Navbars/index.js
@@ -1,43 +1,43 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { NavIconsList } from '../../Constant/Navbar';
 import NavItemsList from './NavItemsList';
 import { useNavigate } from "react-router-dom";
-import {NavbarContainer} from './styleComponent';
+import { NavbarContainer } from './styleComponent';
 import { useEffect } from "react";
 
 const Navbars = () => {
     const [clickActiveIndex, setClickActiveIndex] = useState(0)
-    const routePath = window.location.pathname ;
-    
-    useEffect(() =>{
+    const routePath = window.location.pathname;
+
+    useEffect(() => {
         if (routePath) {
-            NavIconsList.map(val => {
-                if(routePath === val.path){
-                    setClickActiveIndex(val.index -1)
+            NavIconsList.forEach(val => {
+                if (routePath === val.path) {
+                    setClickActiveIndex(val.index - 1);
                 }
-           } )    
+            });
         }
-    },[routePath])
+    }, [routePath]);
     const navigate = useNavigate()
     const handlePath = (path, index) => {
         navigate(`${path}`)
         setClickActiveIndex(index)
     }
     return (
-      <NavbarContainer
-  display={routePath === '/' ? 'none' : 'flex'}
-  className="no-gutters justify-content-center align-items-center"
->
-  {NavIconsList.map((val, index) => (
-    <NavItemsList
-      key={index}
-      handlePath={handlePath}
-      val={val}
-      index={index}
-      clickActiveIndex={clickActiveIndex}
-    />
-  ))}
-</NavbarContainer>
+        <NavbarContainer
+            display={routePath === '/' ? 'none' : 'flex'}
+            className="no-gutters justify-content-center align-items-center"
+        >
+            {NavIconsList.map((val, index) => (
+                <NavItemsList
+                    key={index}
+                    handlePath={handlePath}
+                    val={val}
+                    index={index}
+                    clickActiveIndex={clickActiveIndex}
+                />
+            ))}
+        </NavbarContainer>
     )
 }
 export default Navbars;
